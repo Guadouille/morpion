69dca09a3fc882a9b4ea6f1e21777aa25e0e1dad
Guadouille <juju.fafnyr@gmail.com>
2020-05-24 19:03:27 +0200

    first line displayed

--- main.py
+++ main.py
@@ -1,11 +1,38 @@
 import pygame
 
+def playToken(board,key):
+    print(key)
+    if key == pygame.K_KP1:
+        board[2][0] = "1"
+    if key == pygame.K_KP2:
+        board[2][1] = "1"
+    if key == pygame.K_KP3:
+        board[2][2] = "1"
+    if key == pygame.K_KP4:
+        board[1][0] = "1"
+    if key == pygame.K_KP5:
+        board[1][1] = "1"
+    if key == pygame.K_KP6:
+        board[1][2] = "1"
+    if key == pygame.K_KP7:
+        board[0][0] = "1"
+    if key == pygame.K_KP8:
+        board[0][1] = "1"
+    if key == pygame.K_KP9:
+        board[0][2] = "1"
+    print(board)
+
+
+board=[[0,0,0], [0,0,0], [0,0,0]]
+
 pygame.init()
 
 pygame.display.set_caption("morpion")
-screen = pygame.display.set_mode((450,450))
+screen = pygame.display.set_mode((400,400))
 running = True
 background = pygame.image.load("assets/grille.png")
+rond= pygame.image.load("assets/O.png")
+croix= pygame.image.load("assets/X.png")
 white = ((255,255,255))
 screen.fill(white)
 pygame.display.flip()
@@ -13,11 +40,24 @@ pygame.display.flip()
 while running:
     #le background
     screen.fill(white)
-    screen.blit(background, (25,25))
-    #metre a jour l'ecran
-    pygame.display.flip()
+    screen.blit(background, (0,0))
+    #dispaly board
+    if board[2][0] == "1":
+        screen.blit(rond, (0,300))
+    if board[2][1] == "1":
+        screen.blit(rond, (150,300))
+    if board[2][2] == "1":
+        screen.blit(rond, (300,300))
+
     for event in pygame.event.get():
 
         if event.type == pygame.QUIT:
             running = False
             pygame.quit()
+
+        if event.type == pygame.KEYDOWN:
+            key = event.key
+            playToken(board,key)
+
+    #metre a jour l'ecran
+    pygame.display.flip()
