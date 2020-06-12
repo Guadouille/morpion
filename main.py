b3f16ebcf32976e08bdf7fd81a0558b859a33f0d
Guadouille <juju.fafnyr@gmail.com>
2020-06-12 15:34:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -128,6 +128,40 @@ while running:
                 player= 1
 
 
-
-    #metre a jour l'ecran
+#afficher le vainqueur
+#rond
+    if board[0][0],[0][1],[0][2] == 1 :
+        lagagne = pygame.image.load("Gagnant.png")
+    if board[1][0],[1][1],[1][2] == 1 :
+        lagagne
+    if board[2][0],[2][1],[2][2] == 1 :
+        lagagne
+    if board[0][0],[1][0],[2][0] == 1 :
+        lagagne
+    if board[0][1],[1][1],[2][1] == 1 :
+        lagagne
+    if board[0][2],[1][2],[2][2] == 1 :
+        lagagne
+    if board[0][0],[1][1],[2][2] == 1 :
+        lagagne
+    if board[0][2],[1][1],[2][0] == 1 :
+        lagagne
+#croix
+    if board[0][0],[0][1],[0][2] == 2 :
+        lagagne
+    if board[1][0],[1][1],[1][2] == 2 :
+        lagagne
+    if board[2][0],[2][1],[2][2] == 2 :
+        lagagne
+    if board[0][0],[1][0],[2][0] == 2 :
+        lagagne
+    if board[0][1],[1][1],[2][1] == 2 :
+        lagagne
+    if board[0][2],[1][2],[2][2] == 2 :
+        lagagne
+    if board[0][0],[1][1],[2][2] == 2 :
+        lagagne
+    if board[0][2],[1][1],[2][0] == 2 :
+        lagagne
+        #metre a jour l'ecran
     pygame.display.flip()
